70a5050e27238a7549d9cb71ee016887b86c4d1a
Evan Rowbotham <rowbotham.evan@gmail.com>
2025-06-01 01:02:19 -0400

    fix thumbnail, 404 error, and interest blurb on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -100,7 +100,7 @@ export default function Home() {
       <p>
         I was an applied math major at Florida State University until 2022. I just transferred to NYU as a mathematics major. (they don’t have applied)
         My interests are wide ranging, so it’s more accurate to say I just enjoy talking to thoughtful, committed, and resilient people who push themselves to constantly grow, refine their world views, and give to others. (I especially enjoy learning about other people’s cultures!)
-        My most current side obsession is learning about the different “interpretations” (and thus different ontological commitments) of quantum mechanics.
+        My most current side obsession is learning about the different "interpretations" (and thus different ontological commitments) of quantum mechanics. 
       </p>
       <p>
         Current fascination: competing ontologies in quantum mechanics.

7ca48f0cd580deaf04d75c4f7f69b8468290c71d
Evan Rowbotham <rowbotham.evan@gmail.com>
2025-06-01 18:27:07 -0400

    vite reversion

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mdx from '@mdx-js/rollup'; 
+import mdx from '@mdx-js/rollup';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/website/', // <--- THIS IS CRUCIAL if deployed to a subfolder named 'website'
-  build: {
-    outDir: 'dist', // Default output directory for the build
-  },
+  plugins: [
+    react(),
+    mdx({
+      providerImportSource: '@mdx-js/react',
+      jsxRuntime: 'automatic'
+    })
+  ]
 });

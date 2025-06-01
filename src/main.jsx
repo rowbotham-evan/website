5f53206238f07f88188f32a32004c296b89fe069
Evan Rowbotham <rowbotham.evan@gmail.com>
2025-06-01 00:49:43 -0400

    main baseline fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-// src/main.jsx   ← single entry file used by index.html
-import ReactDOM        from "react-dom/client";
+// src/main.jsx
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { MDXProvider }   from "@mdx-js/react";
-import App               from "./App";
+import { MDXProvider } from "@mdx-js/react";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/website"> 
     <MDXProvider>
       <App />
     </MDXProvider>

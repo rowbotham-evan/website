4a9298f89131b284305e7d01bc22463a43443985
Evan Rowbotham <rowbotham.evan@gmail.com>
2025-06-01 01:04:42 -0400

    fixing homepage blurb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -99,11 +99,12 @@ export default function Home() {
 
       <p>
         I was an applied math major at Florida State University until 2022. I just transferred to NYU as a mathematics major. (they don’t have applied)
-        My interests are wide ranging, so it’s more accurate to say I just enjoy talking to thoughtful, committed, and resilient people who push themselves to constantly grow, refine their world views, and give to others. (I especially enjoy learning about other people’s cultures!)
-        My most current side obsession is learning about the different "interpretations" (and thus different ontological commitments) of quantum mechanics. 
       </p>
       <p>
-        Current fascination: competing ontologies in quantum mechanics.
+        My interests are wide ranging, so it’s more accurate to say I just enjoy talking to thoughtful, committed, and resilient people who push themselves to constantly grow, refine their world views, and give to others. (I especially enjoy learning about other people’s cultures!)
+      </p>
+      <p>
+        My most current side obsession is learning about the different "interpretations" (and thus different ontological commitments) of quantum mechanics. 
       </p>
       <p style={{ marginTop: 24 }}>
         <NavLink to="https://calendly.com/rowbotham-evan/coffee-chat"
